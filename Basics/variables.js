cead6f5bf66312965c52cfd82e3b9e42ba5ea77d
Parvez Mujawar <prvzmujawar@gmail.com>
2024-04-24 07:58:06 +0000

    Added Variables.js file

---
+++ Basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 1000
+let accountEmail = "hello@gmail.com"
+var accountPassword = "202020"
+accountcity = "bengaluru"
+
+//let us change the values!!
+accountEmail = "prvz@gmail.com"
+accountPassword = "10000"
+accountcity = "vijayapur"
+
+//Output all at once
+console.table([accountEmail, accountPassword, accountcity])
