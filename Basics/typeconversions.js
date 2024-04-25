88cf4daf28fc34be3bb57ca2ad1b8cd413597aca
Parvez Mujawar <prvzmujawar@gmail.com>
2024-04-25 06:21:01 +0000

    Added typeconversions.js file

---
+++ Basics/typeconversions.js
@@ -0,0 +1,27 @@
+let score = "Parvez"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+// "33" => 33 to convert string to number
+//"33abc" => NaN
+//true = 1; false = 0;
+// "" => false ()
+// "parvez" => true
+
+let isLoggedIn = "parvez"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
