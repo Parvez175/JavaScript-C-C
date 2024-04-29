c850935ee23d16452bd2b8ca8e2a9d13adbb94ae
Parvez Mujawar <prvzmujawar@gmail.com>
2024-04-29 16:22:35 +0000

    Added memoryStack-Heap.js file

---
+++ Basics/memoryStack-Heap.js
@@ -0,0 +1,23 @@
+//stack(Primitive) Heap(Non-Primitive)
+
+let = mynameis = "parvez"
+ let myAnotherNameis = "Ali"
+ myAnotherNameis = "Chaiislove"
+
+ console.log(myAnotherNameis);
+
+ console.log(mynameis);
+ 
+
+Heap(Non-Primitive)
+let user1 = {
+    email : "Parvez@gmail.com",
+    upi : "parvez@ybl"
+}
+
+let user2 = user1
+
+user2.email = "parvezmu@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
