749ea2cd16afa53551adb24a22a46d0ca1c325d5
Parvez Mujawar <prvzmujawar@gmail.com>
2024-05-02 12:28:23 +0000

    Added functions.file

--- Basics/functions.js
+++ Basics/functions.js
@@ -20,7 +20,7 @@ function addnumber(number1, number2) {
     return number1 + number2
 }
 const result = addnumber(4, 5)
-console.log("result", result);
+// console.log("result", result);
 
 
 function addanother(username) {
@@ -29,6 +29,42 @@ function addanother(username) {
         return
     }
     return `${username} just logged in `
-}
+
 
 console.log(addanother()); 
+
+// ++++++++++++++++++++Second Part ++++++++++++++++++++++++
+
+ function AddCart (val1, val2, ...num1){
+    return num1 
+ }
+
+ console.log(AddCart(2,3,4,5,6)); 
+
+const user ={
+    name : "Parvez",
+    age : 22
+}
+
+function handleObject (anyobject){
+    console.log(`username is ${anyobject.name} and my age is ${anyobject.age}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username : "Parvez",
+    age : 33
+})
+
+
+const mynewarray = [100,200,300]
+
+function newarray (getarray){
+    return getarray[0]
+
+}
+
+console.log(newarray(mynewarray))
+
+}
