d0945670c0919bfd051fb67539256effa5cff4b3
Parvez Mujawar <prvzmujawar@gmail.com>
2024-05-03 13:49:43 +0000

    Added  Control_Flow.js file

--- Basics/Control_Flow.js
+++ Basics/Control_Flow.js
@@ -2,105 +2,104 @@
 
 const temperature = 50
 
- if (temperature === 40) {
+if (temperature === 40) {
     console.log(`Iam goin to watch Boys Series on amazon prime on june 12`);
- }
- else {
+}
+else {
     console.log(`Iam not going to watch the season on june 12`);
 
 
-const balance = 1200
+    const balance = 1200
 
-if (balance < 500) {
-    console.log(`less than 500 `);
-}else if (balance < 750){
-    console.log(`less than 750 `);
-}else if (balance < 1000){
-console.log(`less than 1000`);
-}else (`Equal to 1200`);{
-    console.log(`Equal to 1200`);
+    if (balance < 500) {
+        console.log(`less than 500 `);
+    } else if (balance < 750) {
+        console.log(`less than 750 `);
+    } else if (balance < 1000) {
+        console.log(`less than 1000`);
+    } else (`Equal to 1200`); {
+        console.log(`Equal to 1200`);
+    }
+
+
+    const IsuserLoggedn = true
+    const Debitcard = true
+    const Email = true
+    //  const Google = false
+
+    if (IsuserLoggedn && Debitcard) {
+        console.log(`User has logged In`);
+
+    }
+
+
+    if (Email || Google) {
+        console.log(`user has Logged In with Either email  or google`);
+
+    }
+
+
+    switch (key) {
+        case value:
+
+            break;
+
+        default:
+            break;
+    }
+    const month = 3
+
+    switch (month) {
+        case 1:
+            console.log("January");
+            break;
+        case 2:
+            console.log("Feb");
+            break;
+        case 3:
+            console.log("March");
+            break;
+        case 1:
+            console.log("April");
+            break;
+
+        default:
+            console.log("Deault match");
+            break;
+    }
+
+
+    // Falsy Values 
+
+    // false, 0, -0, BigInt 0n, Undefined, Null, NaN, ""
+
+    // Truthy Values
+
+    "0(zero)", "false", " ", [], {}, function () { }
+
+    const Obj = {}
+
+    if (Object.keys(Obj).length === 0) {
+        console.log("Empty Object");
+    }
+
+    // Nullish Coalesing Operator (??) : Null, Undefined
+
+    let val1;
+
+    val1 = 10 ?? 20
+    val1 = null ?? 30
+    val1 = undefined ?? 40
+
+    console.log(val1);
+
+
+    //Ternary Operator
+
+    const iceTeaPrice = 100
+    iceTeaPrice <= 80 ? console.log("less than 80") : console.log("More then 80")
+
 }
-
-
- const IsuserLoggedn = true
- const Debitcard = true
- const Email = true
-//  const Google = false
-
- if (IsuserLoggedn && Debitcard) {
-    console.log(`User has logged In`);
-    
- }
-
-
- if (Email || Google) {
-    console.log(`user has Logged In with Either email  or google`);
-    
- }
-
-
-switch (key) {
-    case value:
-        
-        break;
-
-    default:
-        break;
-}
-const month = 3
-
-switch (month) {
-    case 1:
-        console.log("January");
-        break;
-    case 2:
-        console.log("Feb");
-        break;
-    case 3:
-        console.log("March");
-        break;
-    case 1:
-        console.log("April");
-        break;
-
-    default:
-        console.log("Deault match");
-        break;
-}
-
-
-// Falsy Values 
-
-false, 0, -0, BigInt 0n, Undefined, Null, NaN, ""
-
-// Truthy Values
-
-"0(zero)", "false", " ", [], {}, function(){}
-
-const Obj = {}
-
-if (Object.keys(Obj).length===0) {
-    console.log("Empty Object");
-}
-
-// Nullish Coalesing Operator (??) : Null, Undefined
-
-let val1;
-
-val1 = 10 ?? 20
-val1 = null ?? 30
-val1 = undefined ?? 40
-
-console.log(val1);
-
-
-//Ternary Operator
-
-const iceTeaPrice = 100
-iceTeaPrice <=80 ? console.log("less than 80") : console.log("More then 80");
-
-
-
 
 
 
