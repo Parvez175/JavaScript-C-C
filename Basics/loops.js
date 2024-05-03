e4263fa56bec8c4d7c3b25e2b123ef81238ddc69
Parvez Mujawar <prvzmujawar@gmail.com>
2024-05-03 16:17:45 +0000

    Added loops.js

---
+++ Basics/loops.js
@@ -0,0 +1,52 @@
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    if (index==5) {
+        // console.log("5 is Best Number");
+    }
+    // console.log(element);
+    
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`outer loop value : ${i}`);
+    for (let j = 1; j <= 10; j++) {
+    // console.log(`Inner loop value : ${j} `);
+    // console.log(i + "*"  + j + "=" + i*j);
+        
+    }
+    
+}
+
+let myArray =  ["Superman", "Homelander", "Batman"]
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+for (let i = 1; i < 20; i++) {
+    if (i==5) {
+        console.log(`Detected Number 5`);
+    }
+    console.log(`Print value : ${i}`);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
