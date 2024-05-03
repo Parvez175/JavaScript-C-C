d2b7344c9ac44d83ec13d964a116a52f3a9b6c93
Parvez Mujawar <prvzmujawar@gmail.com>
2024-05-03 16:41:06 +0000

    Added loops.js file

---
+++ Basics/loops_02.js
@@ -0,0 +1,27 @@
+//Do and While loop
+
+// let myValue = 0
+
+// while (myValue < 10) {
+//     console.log(`Print Value : ${myValue}`);
+//     myValue =  myValue + 3
+    
+// }
+
+let mySuperHeros = ["Homelander", "Ironman", "Black Panther"]
+
+let arr = 0
+
+while (arr < mySuperHeros.length) {
+    // console.log(`Print Value : ${mySuperHeros[arr]}`);
+    arr = arr + 1
+}
+
+//Do While loop
+
+let score = 0
+
+do {
+    console.log(`Print value ${score}`);
+    score ++
+} while (score <= 20);
