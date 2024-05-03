fdb06b1fe57628d3bf0265d12969c30571c9c723
Parvez Mujawar <prvzmujawar@gmail.com>
2024-05-03 06:29:54 +0000

    Added arrows.js file

---
+++ Basics/arrows.js
@@ -0,0 +1,46 @@
+// function chai (){
+//     console.log(this);
+// }
+
+// // chai()
+
+// const user = {
+//     name : "Parvez",
+//     age : 22,
+//     welcomeMessage : function(){
+//         console.log(`$(name), welcome my Boy!!`);
+//         // console.log(this);
+
+//     }
+// }
+
+
+// // console.log(user.welcomeMessage);
+// // console.log(user.name);
+
+
+
+// const chai = () => {
+// let username = "Parvez"
+//     console.log(this)
+// }
+
+// // chai()
+
+
+const addTwo = (num1, num2) => (num1 +num2) //Implicit return
+
+//If we use return Keyword => Explicit return
+
+
+console.log(addTwo(2,3));
+
+const addTwoo = (num1, num2) => ({username : "Parvezz"}) 
+
+console.log(addTwoo(3,5));
+
+
+
+
+
+
