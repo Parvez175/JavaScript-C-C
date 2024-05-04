d15b772e6a24cb25696d02c456ead9f286bf7229
Parvez Mujawar <prvzmujawar@gmail.com>
2024-05-04 19:03:02 +0000

    Added DOM.js file

---
+++ Basics/DOM.js
@@ -0,0 +1,56 @@
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="UTF-8" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    {/* <title>Document</title>
+  </head>
+  <body style="background-color: black; color: aliceblue">
+    <div class="parent">
+      <div class="day">Moanday</div>
+      <div class="day">Tuesday</div>
+      <div class="day">Wednesday</div>
+      <div class="day">Thursday</div>
+    </div>
+  </body> */}
+
+  <script>
+    const parent = document.querySelector(".parent");
+    // console.log(parent);
+    // console.log(parent.children);
+    // console.log(parent.children[2].innerHTML);
+
+    {/* for (let i = 0; i < parent.children.length; i++) { */}
+      // console.log(parent.children[i].innerHTML)
+      
+    {/* } */}
+
+    // parent.children[1].style.color = "yellow"
+    // console.log(parent.firstElementChild);
+    // console.log(parent.lastElementChild);
+
+
+    // const dayone = document.querySelector(".day")
+    // console.log(dayone);
+    // console.log(dayone.parentElement);
+    // console.log(dayone.nextElementSibling);
+    // console.log("Nodes", parent.childNodes );
+
+
+{/* 
+    const div = document.createElement("div"); 
+    console.log(div);
+    div.className= "main"
+    div.id = Math.round(Math.random() * 10 + 1)
+
+    div.setAttribute("title", "generated title")
+    div.style.backgroundColor = "green"
+    div.style.padding = "10px" */}
+    {/* // div.innerHTML = "Parrrvez"
+    const addtext = document.createTextNode("Parvez")
+    div.appendChild(addtext)
+
+   document.body.appendChild(div) */}
+
+  </script>
+// </html>
